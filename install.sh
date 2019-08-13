b395bbe3462dbeddb3dc61991ad24dca5cd5d6f5
frederick tyteca <frederick@tyteca.net>
2019-08-13 17:46:39 +0200

    files in home are instaalled properly

--- install.sh
+++ install.sh
@@ -20,15 +20,27 @@ waitForUser() {
     done
 }
 
-for file in $filesToReplace;do
+for file in $filesToReplace; do
     fileInHome="$HOME/$file"
     fileFromRepo="$HOME/dotfiles/$file"
     backupFile="$fileInHome.back"
-    if [ -f $backupFile ]; then
+
+    if [ -L $backupFile ]; then
+        # if backup file already exists AND is one symlink => removing
+        unlink $backupFile
+    elif [ -f $backupFile ]; then
+        # if backup file already exists AND is one regular file => removing
         waitForUser "Backup file $backupFile already exists. Do you agree to overwrite it ? [y/N] : "
         unlink $backupFile
     fi
-    mv $fileInHome $backupFile
+
+    if [ -L $fileInHome ]; then
+        # if file to replace already exists AND is one symlink => removing
+        unlink $fileInHome
+    elif [ -f $fileInHome ]; then
+        # if file to replace already exists AND is one regulaar file => moving
+        mv $fileInHome $backupFile
+    fi 
     ln -s $fileFromRepo $fileInHome
 done
 exit 0

0d31c4a5ccd1ebdb121fba96b39b66b3c48efcd6
frederick tyteca <frederick@tyteca.net>
2019-12-05 16:19:06 +0100

    formatted

--- oh-my-zsh/custom/plugins/laradocker/laradocker.plugin.zsh
+++ oh-my-zsh/custom/plugins/laradocker/laradocker.plugin.zsh
@@ -2,18 +2,18 @@
 # This plugin is useful for those who are running multiple laravel containers.
 # The purpose is being able to run artisan according to the project you are in
 # to use it properly you should respect some requirements.
-# - using container name 
+# - using container name
 # - last part of the folder (where is stored artisan) should have the same name as your container
 #  		/path/../to/<containername>/artisan
 
 function artisan() {
 	# checking if artisan is there
-	if [ ! -f artisan ];then
+	if [ ! -f artisan ]; then
 		echo "not a laravel path"
 		return 1
 	fi
 
-	# get the container name 
+	# get the container name
 	# container should have the same name than the last part of the path
 	# tree structure
 	# /path/to/containerName

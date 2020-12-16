c72e336189276e8c7a1531f83a29d76c6f56264e
frederick tyteca <frederick@tyteca.net>
2020-12-16 17:43:12 +0100

    another install script

--- scripts/install-node-with-npm.sh
+++ scripts/install-node-with-npm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is installing vscode
 
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 sudo apt-get install -y nodejs

147cbc1b9facc37f06d1def0adb9eb8feda419f3
frederick tyteca <frederick@tyteca.net>
2021-10-09 18:17:05 +0200

    adding ngrok alias and update for youtube-dl install script (replaced by yt-dlp).

--- scripts/install-youtube-dl.sh
+++ scripts/install-youtube-dl.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script is installing youtube-dl
 
-sudo apt update -y && sudo apt install --no-install-recommends -y python
+sudo apt update -y && sudo apt install --no-install-recommends -y python ffmpeg
 
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 
-sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/yt-dlp
 
-youtube-dl --version
+yt-dlp --version

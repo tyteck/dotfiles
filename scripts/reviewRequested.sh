a9af10dd0c99766f41b75cd72c51de9b6ca0c704
frederick tyteca <frederick@tyteca.net>
2022-01-18 09:50:20 +0100

    script to get notification when I'm asked a review PR --- draft

---
+++ scripts/reviewRequested.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+# this script will retrieve PR request where I'm asked a review
+# and send me a gnome notification on desktop
+
+nbReviewRequested=$(gh api -X GET search/issues -f q='review:required user-review-requested:@me' | jq '.total_count')
+if [ $nbReviewRequested -ne "0" ]; then
+    notify-send foo bar
+fi

c20241b8e053c50e27232ebb02d1a3671ca02252
frederick tyteca <frederick@tyteca.net>
2022-03-08 17:19:15 +0100

    add northstar shortcut

--- oh-my-zsh/custom/plugins/laradocker/laradocker.plugin.zsh
+++ oh-my-zsh/custom/plugins/laradocker/laradocker.plugin.zsh
@@ -49,6 +49,8 @@ function getDockerPrefix() {
         dockerPrefix="docker-compose -f ${LUCIE_PATH}/docker/docker-compose.yml -p actual exec php-nginx "
     elif [[ "$lastFolderName" = "nina" || "$lastFolderNames" = "nina/app" ]]; then # nina - Actual
         dockerPrefix="docker-compose -f ${NINA_PATH}/build/docker-compose.yml -p nina exec -e XDEBUG_MODE=off php-nginx "
+    elif [[ "$lastFolderName" = "northstar" ]]; then # northstar
+        dockerPrefix="docker exec -it app "
     elif isInstalled "docker" && containerExists $lastFolderName; then
         dockerPrefix="docker exec -it --user www-data $lastFolderName "
     fi

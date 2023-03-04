b6fa5468c3ef9e7766632f1727129595658022ef
frederick tyteca <frederick@tyteca.net>
2023-03-04 15:32:15 +0100

    upgrading youtube-dl

--- bin/install-youtube-dl.sh
+++ bin/install-youtube-dl.sh
@@ -1,10 +1,21 @@
-#!/bin/bash
+#!/usr/bin/zsh
 # This script is installing youtube-dl
 
-sudo apt update -y && sudo apt install --no-install-recommends -y python ffmpeg
+# loading coloring message
+. $HOME/dotfiles/coloredMessage.sh
+
+sudo apt update -y && sudo apt install --no-install-recommends -y python3 ffmpeg
+
+if [ -d /usr/local/bin/yt-dlp ]; then
+    # removing "colume" mounted by docker if yt-dlp not present
+    sudo rm -f /usr/local/bin/yt-dlp
+fi
 
 sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 
 sudo chmod a+rx /usr/local/bin/yt-dlp
 
 yt-dlp --version
+if [ $? = 0 ]; then
+    comment "yt-dlp was successfully installed"
+fi

f719302e601b0496492e3fd912b73bfd82071755
frederick tyteca <frederick@tyteca.net>
2023-10-24 10:53:57 +0200

    freemobile improved

--- bin/freemobile.sh
+++ bin/freemobile.sh
@@ -5,21 +5,24 @@
 FREE_URL=https://mobile.free.fr/account/antennes-relais-indisponibles.csv
 FILE_PATH=/tmp/antennes-relais-indisponibles.csv
 
+# pre clean
+rm -f $FILE_PATH
+
 # getting csv file
 wget -O $FILE_PATH $FREE_URL >/dev/null 2>&1
-if [ $? != 0 ];then
+if [ $? != 0 ]; then
     MESSAGE="C'est free ou la connexion qui est down."
     notify-send "FREE-MOBILE" "$MESSAGE"
     exit 1
 fi
 # looking for the ones that interest me
-IN_TROUBLE=$(cat antennes-relais-indisponibles.csv | grep -e "NICE" -e "TOURRETTE LEVENS" -e "ASPREMONT" | wc -l)
+IN_TROUBLE=$(cat $FILE_PATH | grep -e "TOURRETTE LEVENS" -e "ASPREMONT" | wc -l)
 
 MESSAGE="No problemo !"
 ERROR=0
 if [ $IN_TROUBLE -gt 0 ]; then
     MESSAGE="L'antenne de Free est down !"
     ERROR=1
-    notify-send "FREE-MOBILE" "$MESSAGE"
 fi
+notify-send "FREE-MOBILE" "$MESSAGE"
 exit $ERROR

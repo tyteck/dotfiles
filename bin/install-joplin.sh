850a690866644421d8e3e93dcfea68416849d91a
frederick tyteca <frederick@tyteca.net>
2024-03-20 10:16:43 +0100

    install-joplin improved

--- bin/install-joplin.sh
+++ bin/install-joplin.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 # This script is installing joplin
+# it check the installed version in $HOME/.joplin/VERSION
+# download the file Joplin.AppImage in the same folder
 
+symlinkName=Joplin.AppImage
 #
 # Installing joplin
 #
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
 
-echo "you should have a $HOME/.joplin folder"
-echo "run the ~/$HOME/.joplin/Joplin.AppImage and choose integrate & run (AppImageLauncher)"
-echo "then you have to recreate the Joplin.AppImage symlink in $HOME/Applications"
+echo "about to run AppImageLauncher, you need to click on integrate and run."
+read -s -k '?Press any key to continue.'
 
-exit 0
+# get current symlink target
+currentAppImage=$(readlink -f $symlinkName)
+
+# removing current version
+rm -fv $currentAppImage
+
+# removing symlink
+unlink $symlinkName
+
+# getting new version path (should have 1 only)
+newAppImage=$(find $HOME/Applications -type f -name "Joplin*")
+
+ln -s /home/fred/Applications/Joplin_dc9fc77a236a0e2e64b32a4243bc3543.AppImage Joplin.AppImage
+
+AppImageLauncher $HOME/.joplin/Joplin.AppImage

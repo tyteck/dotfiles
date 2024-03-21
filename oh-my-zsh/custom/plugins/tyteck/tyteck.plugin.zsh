883a5439d36e2cd53a6de6f6d9d3bee64d90f2a9
frederick tyteca <frederick@tyteca.net>
2024-03-21 11:12:30 +0100

    minor update

--- oh-my-zsh/custom/plugins/tyteck/tyteck.plugin.zsh
+++ oh-my-zsh/custom/plugins/tyteck/tyteck.plugin.zsh
@@ -201,7 +201,7 @@ function apacheandme() {
         if [ -f $FILE ]; then
             sudo chown www-data:$USER $FILE && sudo chmod g+w $FILE
         elif [ -d $FILE ]; then
-            sudo chown -R www-data:$USER $FILE && sudo chmod g+w $FILE
+            sudo chown -R www-data:$USER $FILE && sudo chmod -R g+w $FILE
         else
             echo "{$FILE} do not exists. Check your path !"
             continue
